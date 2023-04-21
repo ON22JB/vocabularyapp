8f2abc6c0e3b36df76cfa677b65d7a9bcea96891
ON22JB <jacqbohn@aol.com>
2023-04-21 10:48:39 +0200

    Adding const and interface for Vocab in En/De

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,30 @@
+//Explanation:
+//"En" for English
+//"De" for German
 
+//Adding new Vocabulary
+const newVocabEnInput = document.querySelector("#newVocabEnInput") as HTMLInputElement; 
+const newVocabDeInput = document.querySelector("#newVocabDeInput") as HTMLInputElement;
+const newVocabBtn = document.querySelector("#newVocabBtn") as HTMLButtonElement;
+
+//Check if correct input or not with message
+const validatorMessage = document.querySelector("#validatorMessage") as HTMLSpanElement;
+
+//Container to show Vocabulary list
+const vocabEnContainer = document.querySelector("#vocabEnContainer") as HTMLDivElement;
+const vocabDeContainer = document.querySelector("#vocabDeContainer") as HTMLDivElement;
+
+//Information to save inside of each Vocabulary (En & De)
+interface VocabEn {
+  description: string; //the word
+  id: string; //randomized ID
+  timestamp: Date;
+}
+interface VocabDe {
+  description: string;
+  id: string;
+  timestamp: Date;
+}
 
 
 
