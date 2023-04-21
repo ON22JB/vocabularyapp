5ff45474acca8c22147904b05ea89d2a406a26b5
ON22JB <jacqbohn@aol.com>
2023-04-21 11:12:05 +0200

    Add new functions to main function

--- src/index.ts
+++ src/index.ts
@@ -110,6 +110,9 @@ function addVocabDe() {
 //main function
 function initApp() {
   newVocabBtn.disabled = true;
+  newVocabBtn.addEventListener("click", addVocabEn);
+  newVocabEnInput.addEventListener("input", validateInput);
+  newVocabDeInput.addEventListener("input", validateInput);
 
 }
 
